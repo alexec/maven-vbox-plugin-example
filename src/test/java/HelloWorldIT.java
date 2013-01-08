8aca86e92d4592f46f708bf305058c540215038c
Alex Collins <alex.e.c@gmail.com>
2013-01-08 23:13:02 +0000

    Bug: SCP URL typo. URL typo.

--- src/test/java/HelloWorldIT.java
+++ src/test/java/HelloWorldIT.java
@@ -7,6 +7,6 @@ public class HelloWorldIT {
 
     @Test
     public void testHelloWorld() throws Exception {
-        assertTrue(Jsoup.connect("http://localhost:18080/maven-vbox-plugin-example").get().html().contains("Hello World"));
+        assertTrue(Jsoup.connect("http://192.168.56.2:8080/maven-vbox-plugin-example").get().html().contains("Hello World"));
     }
 }
